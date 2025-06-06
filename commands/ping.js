f51918646306c9ac0276e301cfc684cf968986d4
Tharaks2008 <arundathishashipabha@gmail.com>
2025-06-06 21:37:16 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -29,11 +29,8 @@ async function pingCommand(sock, chatId, message) {
         const uptimeFormatted = formatTime(uptimeInSeconds);
 
         const botInfo = `
-┏━━〔 🤖 𝐊𝐧𝐢𝐠𝐡𝐭𝐁𝐨𝐭-𝐌𝐃 〕━━┓
-┃ 🚀 Ping     : ${ping} ms
-┃ ⏱️ Uptime   : ${uptimeFormatted}
-┃ 🔖 Version  : v${settings.version}
-┗━━━━━━━━━━━━━━━━━━━┛`.trim();
+  🤖 Red-dragan-bot
+🚀 Ping`.trim();
 
         // Reply to the original message with the bot info
         await sock.sendMessage(chatId, { text: botInfo},{ quoted: message });
