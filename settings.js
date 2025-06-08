bcc674baa85fb6d7425456dfe91ec6c93f76b05e
Tharaks2008 <arundathishashipabha@gmail.com>
2025-06-08 16:39:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'RED_DRAGAN_Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "RED_DRAGAN_Bot",
+  botOwner: 'THARAKA', // Your name
+  ownerNumber: '94786908427', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
